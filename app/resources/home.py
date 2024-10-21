7035712b1f12c03a0528123419386dada7d0b900
gvrl1 <ag.moreno@alumno.um.edu.ar>
2024-10-20 22:50:17 -0300

    prueba 1

---
+++ app/resources/home.py
@@ -0,0 +1,10 @@
+from flask import jsonify, Blueprint, request
+
+home = Blueprint('home', __name__)
+
+@home.route('/', methods=['GET'])
+def index():
+    my_ip = request.remote_addr
+    resp = jsonify({"microservicio": my_ip, "status": "ok"})
+    resp.status_code = 200
+    return resp
