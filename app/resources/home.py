8ab7c5d906b4e155e818e7690aa3a74e5f9324ac
gvrl1 <ag.moreno@alumno.um.edu.ar>
2024-11-05 14:40:34 -0300

    OpenTelemetry with AzureMonitor

--- app/resources/home.py
+++ app/resources/home.py
@@ -1,10 +1,21 @@
 from flask import jsonify, Blueprint, request
+from opentelemetry import trace
 
 home = Blueprint('home', __name__)
+tracer = trace.get_tracer(__name__)
 
 @home.route('/', methods=['GET'])
 def index():
-    my_ip = request.remote_addr
-    resp = jsonify({"microservicio": my_ip, "status": "ok"})
-    resp.status_code = 200
-    return resp
+    with tracer.start_as_current_span("index_route") as span:
+        my_ip = request.remote_addr
+        resp = jsonify({"microservicio": my_ip, "status": "ok"})
+        resp.status_code = 200
+        return resp
+
+@home.route('/health', methods=['GET'])
+def health():
+    with tracer.start_as_current_span("health_route") as span:
+        resp = jsonify({"status": "ok"})
+        resp.status_code = 200
+        return resp
+
