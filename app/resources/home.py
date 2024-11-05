d5e474e8703af8fa7bc751b0a6fa70d2d1a9c183
gvrl1 <ag.moreno@alumno.um.edu.ar>
2024-11-05 15:15:13 -0300

    fix #4

--- app/resources/home.py
+++ app/resources/home.py
@@ -5,16 +5,16 @@ home = Blueprint('home', __name__)
 tracer = trace.get_tracer(__name__)
 
 @home.route('/', methods=['GET'])
+@tracer.start_as_current_span("index_route")
 def index():
-    with tracer.start_as_current_span("index_route") as span:
         my_ip = request.remote_addr
         resp = jsonify({"microservicio": my_ip, "status": "ok"})
         resp.status_code = 200
         return resp
 
 @home.route('/health', methods=['GET'])
+@tracer.start_as_current_span("health_route")
 def health():
-    with tracer.start_as_current_span("health_route") as span:
         resp = jsonify({"status": "ok"})
         resp.status_code = 200
         return resp
